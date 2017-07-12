adab0c249b9dbfb09000d376cc85ec017a54f53f
Chaitanya <mnsvkchaitanya@gmail.com>
2017-07-12 09:04:47 +0530

    File Dialog

---
+++ 7-FileDialog/src/PersonFilter.java
@@ -0,0 +1,29 @@
+import java.io.File;
+
+import javax.swing.filechooser.FileFilter;
+
+public class PersonFilter extends FileFilter{
+
+	@Override
+	public boolean accept(File f) {
+		String name = f.getName();
+		int lastIndex = name.lastIndexOf(".");
+		if(f.isDirectory())return true;
+		
+		if(lastIndex == -1 || lastIndex == name.length()-1)
+		return false;
+		
+		else{
+			if(name.substring(lastIndex+1, name.length()-1) == "jpeg")
+				return true;
+			else 
+				return false;
+		}
+	}
+
+	@Override
+	public String getDescription() {
+		return "PersonFie(*.jpeg)";
+	}
+
+}
